3a528e01eddf54f04dae096a42c90dbd582bf18f
candourist <sajadh.ismael@gmail.com>
2024-05-14 23:39:49 +0300

    Completed assignment - QwQ

--- index.js
+++ index.js
@@ -1,14 +1,15 @@
-function isPalindrome(word) {
-  // Write your algorithm here
-}
+const isPalindrome = (word) => {
+  for (let startIndex = 0; startIndex < word.length / 2; startIndex++) {
+    const endIndex = word.length - 1 - startIndex;
+    if (word[startIndex] !== word[endIndex]) {
+      return false;
+    }
+  }
 
-/* 
-  Add your pseudocode here
-*/
+  return true;
+};
 
-/*
-  Add written explanation of your solution here
-*/
+
 
 // You can run `node index.js` to view these console logs
 if (require.main === module) {
